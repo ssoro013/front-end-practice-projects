1c6d03a5cd84e8b088732932fbab2e50a5af3cdc
Soro Didiouf <ssoro013@gmail.com>
2020-01-18 11:43:11 -0800

    Add event listeners

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,1 +1,31 @@
+//matrix: keeps track of x's and o's placement
+var matrix = new Array(3).fill(new Array(3).fill(""));
 
+//players
+var player1 = "X";
+var palyer2 = "O";
+var current = "X";
+
+//add event listeners to table cells
+for (var i = 1; i <= 3; i++) {
+    for (var j = 1; j <= 3; j++) {
+        (function () {
+            var id = `${i}${j}`;
+            var element = document.getElementById(`${id}`);
+            element.addEventListener("click", function() {
+                if (element.innerHTML === "") {
+                    element.innerHTML = current;
+                    current = current === "X" ? "O" : "X";
+                }
+            })
+        }())
+    }
+};
+
+//winner function: determines if there is a winner in the game
+var winner = function(matrix) {
+    
+};
+
+// var sound = new Audio()
+// sound.play()
